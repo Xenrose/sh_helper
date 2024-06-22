43545d8a7bcc15d7c2dec8478a93008b2d0f56c6
Seung Hyun <k_penrose@naver.com>
2024-06-22 09:43:26 +0900

    upload setup

--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='example_package',
+    name='sh_helper',
     version='0.1',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='seung hyun',
+    author_email='sh.kang@alcherainc.com',
     description='A simple example package',
     long_description=open('README.md').read(),
-    url='https://github.com/yourusername/example_package',
+    url='https://github.com/Xenrose/sh_helper',
     packages=find_packages(),
     install_requires=[
         'numpy',
-        'pandas'
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-    ]
+        'pandas',
+        'Pillow',
+        'opencv-python',
+        'shapely']
 )
---
+++ sh_helper/__init__.py
@@ -0,0 +1,1 @@
+from .sh_helper import *
